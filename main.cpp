2a6cbe79e22e0fa715587ef01754905873d1a02f
LidaRP <rahmanipiyani.lida@gmail.com>
2022-02-25 17:13:25 +0330

    do-while

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+          {
+          	int x;
+          	do  
+			{
+				cout<<"Please enter a number:";
+          	    cin>>x;
+          	        for(int i=0;i<x;i++)
+          	        	{
+          	            cout<<"*"<<endl;
+          	            }
+          	        	
+			}while(x!=0);
+return 0;
+          }
